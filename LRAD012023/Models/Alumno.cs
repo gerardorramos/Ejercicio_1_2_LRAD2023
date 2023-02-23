ae9ce4b2285cd6675afba6c80e6e5dc389527b9e
Gerardo <Gerardo@PCGDRR>
2023-02-23 14:24:01 -0600

    Agregar archivos de proyecto.

---
+++ LRAD012023/Models/Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRAD012023.Models
+{
+    public class Alumno
+    {
+        int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set;}
+        public string edad { get; set; }
+        public string correo { get; set;}
+       
+    }
+}
---
+++ LRAD012023/Views/PageAlum.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LRAD012023.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageAlum : ContentPage
+    {
+        public PageAlum()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnmostrar_Clicked(object sender, EventArgs e)
+        {
+            var alum = new Models.Alumno
+            {
+                nombres = txtnombre.Text,
+                apellidos = txtapellidos.Text,
+                edad = txtedad.Text,
+                correo = txtcorreo.Text
+            };
+
+
+            var page = new Views.PageResultado();
+            page.BindingContext= alum;
+            await Navigation.PushAsync(page);
+
+        }
+    }
+}
